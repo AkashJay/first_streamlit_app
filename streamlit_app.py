c54645e8befbe5bfef0575ed2971932af3ea2c45
Akash Jayaweera <41968461+AkashJay@users.noreply.github.com>
2022-12-11 08:38:02 +0530

    Update streamlit_app.py
    Added more content.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('New Healthy Dinner')
+
+streamlit.header('Brealfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeat')
+streamlit.text('Kale, Spinach & Rocket Smothie')
+streamlit.text('Hard-Boiled Free-Tange Egg')
