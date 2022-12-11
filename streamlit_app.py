b282b927620add056eab73b33519029e10b27901
Akash Jayaweera <41968461+AkashJay@users.noreply.github.com>
2022-12-11 08:41:03 +0530

    Update streamlit_app.py
    Added emojies

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('New Healthy Dinner')
 
-streamlit.header('Brealfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeat')
-streamlit.text('Kale, Spinach & Rocket Smothie')
-streamlit.text('Hard-Boiled Free-Tange Egg')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
